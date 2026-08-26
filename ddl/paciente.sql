2e68ec44959853b1737bc158528a345b071456eb
Pedro Olimpio <140788505+olmpio@users.noreply.github.com>
2026-08-25 23:20:08 -0300

    Criação da Tabela Paciente

---
+++ ddl/paciente.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Paciente (
+    id INT,
+    nome VARCHAR(100),
+    cpf VARCHAR(14),
+    data_nascimento DATE,
+    telefone VARCHAR(20),
+    email VARCHAR(100),
+    endereco VARCHAR(150),
+    ativo BOOLEAN
+);
