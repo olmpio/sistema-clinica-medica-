04f55b82a830f246d8f2e0f4c89c0d1a75f65593
Pedro Olimpio <140788505+olmpio@users.noreply.github.com>
2026-08-25 23:26:43 -0300

    Criação da tabela consulta

---
+++ ddl/consulta.sql
@@ -0,0 +1,9 @@
+CREATE TABLE consulta (
+    id INT,
+    data_consulta DATE,
+    horario TIME,
+    motivo VARCHAR(255),
+    diagnostico VARCHAR(255),
+    observacoes VARCHAR(500),
+    status VARCHAR(30)
+);
