007bd922668550d80ddd781afe9fc2959c28f800
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-09 16:23:43 -0300

    Update receita.sql

--- ddl/receita.sql
+++ ddl/receita.sql
@@ -1,5 +1,10 @@
 CREATE TABLE receita (
-    id INT,
-    data_receita DATE,
-    instrucoes VARCHAR(500)
+    id SERIAL PRIMARY KEY,
+    id_consulta INT NOT NULL UNIQUE,
+    data_receita DATE NOT NULL,
+    instrucoes VARCHAR(500),
+    CONSTRAINT fk_receita_consulta
+        FOREIGN KEY (id_consulta)
+        REFERENCES consulta(id)
+        ON DELETE CASCADE
 );
