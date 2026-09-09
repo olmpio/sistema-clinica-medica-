b7f4d91138bb7328f76a4127afd526d1f98eefd5
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-09 16:18:05 -0300

    Update exame.sql

--- ddl/exame.sql
+++ ddl/exame.sql
@@ -1,6 +1,6 @@
 CREATE TABLE exame (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+    nome VARCHAR(100) NOT NULL UNIQUE,
     descricao VARCHAR(255),
-    valor DECIMAL(10,2) NOT NULL CHECK (valor >= 0)
+    valor DECIMAL(10,2) NOT NULL CHECK (valor > 0)
 );
