8f08a92c5a6e338cf0d09e10dfee419086de4cba
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-09 13:57:04 -0300

    Create insert_medicamento.sql

---
+++ dml/insert_medicamento.sql
@@ -0,0 +1,4 @@
+INSERT INTO medicamento (nome, principio_ativo, fabricante) VALUES
+('Paracetamol', 'Paracetamol', 'Medley'),
+('Amoxicilina', 'Amoxicilina Tri-hidratada', 'Eurofarma'),
+('Atenolol', 'Atenolol', 'EMS');
