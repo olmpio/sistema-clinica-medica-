3463763ba6e53acf7423d86512de1724b3b7c853
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-09 13:56:30 -0300

    Create insert_exame.sql

---
+++ dml/insert_exame.sql
@@ -0,0 +1,4 @@
+INSERT INTO exame (nome, descricao, valor) VALUES
+('Hemograma Completo', 'Análise geral das células do sangue', 45.00),
+('Eletrocardiograma', 'Avaliação do ritmo e condução cardíaca', 120.00),
+('Raio-X de Tórax', 'Imagem radiográfica dos pulmões e coração', 90.00);
