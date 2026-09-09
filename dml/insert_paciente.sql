9d267f917f0804cdd48fcd2bd13a0130bfdd2f32
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-09 16:06:38 -0300

    Update insert_paciente.sql

--- dml/insert_paciente.sql
+++ dml/insert_paciente.sql
@@ -1,10 +1,11 @@
-INSERT INTO paciente (nome, cpf, data_nascimento, telefone, email, endereco, id_convenio)
-SELECT 
+INSERT INTO paciente (nome, cpf, data_nascimento, telefone, email, endereco, ativo, id_convenio)
+SELECT
     'Paciente ' || gs,
-    LPAD(gs::text, 11, '0'),
-    DATE '1980-01-01' + (gs || ' days')::interval,
-    '(75) 988' || LPAD(gs::text, 5, '0'),
-    'paciente' || gs || '@meddata.com',
-    'Rua das Flores, ' || gs,
-    (gs % 3) + 1
+    '000.' || LPAD(gs::text, 3, '0') || '.000-' || LPAD((gs % 100)::text, 2, '0'),
+    DATE '1960-01-01' + (gs * 200 || ' days')::interval,
+    '(75) 9' || LPAD((20000000 + gs)::text, 8, '0'),
+    'paciente' || gs || '@email.com',
+    'Rua ' || gs || ', Feira de Santana - BA',
+    TRUE,
+    CASE WHEN gs % 5 = 0 THEN NULL ELSE ((gs - 1) % 5) + 1 END
 FROM generate_series(1, 30) AS gs;
