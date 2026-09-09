e1d76cb59141ad604ba33ef7d2724e1f8a69ef89
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-09 16:15:55 -0300

    Update convenio.sql

--- ddl/convenio.sql
+++ ddl/convenio.sql
@@ -1,7 +1,7 @@
 CREATE TABLE convenio (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    numero_registro VARCHAR(50) UNIQUE,
+    numero_registro VARCHAR(50) NOT NULL UNIQUE,
     telefone VARCHAR(20),
     ativo BOOLEAN NOT NULL DEFAULT TRUE
 );
