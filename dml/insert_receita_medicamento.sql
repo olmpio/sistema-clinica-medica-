6d888daf022a62ba191ec0ec612cfb2a3d5cd587
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-09 16:09:24 -0300

    Update insert_receita_medicamento.sql

--- dml/insert_receita_medicamento.sql
+++ dml/insert_receita_medicamento.sql
@@ -1,8 +1,12 @@
-INSERT INTO receita_medicamento (dosagem, frequencia, duracao, id_receita, id_medicamento)
-SELECT 
-    '500mg',
-    'A cada 8 horas',
-    '7 dias',
+INSERT INTO receita_medicamento (id_receita, id_medicamento, dosagem, frequencia, duracao)
+SELECT
     gs,
-    (gs % 3) + 1
+    ((gs - 1) % 8) + 1,
+    (10 + (gs % 5) * 10) || 'mg',
+    CASE (gs % 3)
+        WHEN 0 THEN '8/8h'
+        WHEN 1 THEN '12/12h'
+        ELSE '1x ao dia'
+    END,
+    (5 + (gs % 10)) || ' dias'
 FROM generate_series(1, 30) AS gs;
