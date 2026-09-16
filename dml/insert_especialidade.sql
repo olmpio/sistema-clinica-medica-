db9b61a875e5441e2435b07e0a6cea466538706a
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-16 20:29:49 -0300

    Update insert_especialidade.sql

--- dml/insert_especialidade.sql
+++ dml/insert_especialidade.sql
@@ -1,4 +1,7 @@
 INSERT INTO especialidade (nome, descricao) VALUES
-('Cardiologia', 'Cuidados com o coração e sistema circulatório'),
-('Ortopedia', 'Tratamento de ossos, articulações e músculos'),
-('Pediatria', 'Atendimento médico infantil e juvenil');
+('Cardiologia', 'Cuidados com o coracao e sistema circulatorio'),
+('Dermatologia', 'Cuidados com a pele, cabelo e unhas'),
+('Ortopedia', 'Cuidados com ossos, musculos e articulacoes'),
+('Pediatria', 'Cuidados medicos infantis'),
+('Ginecologia', 'Saude do sistema reprodutor feminino'),
+('Neurologia', 'Cuidados com o sistema nervoso');
