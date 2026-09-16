711ebe284739396bbc25e0adb7842f1ece15dd66
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-16 20:32:26 -0300

    Update insert_medicamento.sql

--- dml/insert_medicamento.sql
+++ dml/insert_medicamento.sql
@@ -1,4 +1,12 @@
 INSERT INTO medicamento (nome, principio_ativo, fabricante) VALUES
+('Paracetamol', 'Paracetamol', 'EMS'),
+('Amoxicilina', 'Amoxicilina Triidratada', 'Eurofarma'),
+('Ibuprofeno', 'Ibuprofeno', 'Medley'),
+('Omeprazol', 'Omeprazol', 'Neo Quimica'),
+('Losartana', 'Losartana Potassica', 'EMS'),
+('Metformina', 'Cloridrato de Metformina', 'Medquimica'),
+('Dipirona', 'Dipirona Sodica', 'Hipolabor'),
+('Loratadina', 'Loratadina', 'Cimed');INSERT INTO medicamento (nome, principio_ativo, fabricante) VALUES
 ('Paracetamol', 'Paracetamol', 'Medley'),
 ('Amoxicilina', 'Amoxicilina Tri-hidratada', 'Eurofarma'),
 ('Atenolol', 'Atenolol', 'EMS');
