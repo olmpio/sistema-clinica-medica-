c2dd2e9dc9fa315b7ec8ae04bd0bddcadd0e22ff
Edu-almeida554 <edualmeida554@gmail.com>
2026-09-16 20:31:03 -0300

    Update insert_convenio.sql

--- dml/insert_convenio.sql
+++ dml/insert_convenio.sql
@@ -1,4 +1,6 @@
-INSERT INTO convenio (nome, numero_registro, telefone) VALUES
-('Unimed', 'ANS-001234', '(75) 3333-1111'),
-('Bradesco Saúde', 'ANS-005678', '(75) 3333-2222'),
-('Amil', 'ANS-009999', '(75) 3333-3333');
+INSERT INTO convenio (nome, numero_registro, telefone, ativo) VALUES
+('Unimed', 'UNI-0001', '(71) 3333-1111', TRUE),
+('Bradesco Saude', 'BRA-0002', '(71) 3333-2222', TRUE),
+('SulAmerica', 'SUL-0003', '(71) 3333-3333', TRUE),
+('Amil', 'AMI-0004', '(71) 3333-4444', TRUE),
+('Hapvida', 'HAP-0005', '(71) 3333-5555', FALSE);
